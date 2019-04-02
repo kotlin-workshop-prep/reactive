99b057f92e75f71e32abdf9ad1c2644c83fa55e4
aquaraga <aquaraga@gmail.com>
2019-04-02 21:35:21 +0530

    New domain classes

---
+++ src/main/kotlin/domain/Employee.kt
@@ -0,0 +1,13 @@
+package domain
+
+data class Employee(var id: Int, var name: String, var salary: Int) {
+    var organizationId: Int? = null
+    var departmentId: Int? = null
+    constructor(id: Int, name: String, salary: Int, organizationId: Int, departmentId: Int) : this(id, name, salary) {
+        this.organizationId = organizationId
+        this.departmentId = departmentId
+    }
+    constructor(id: Int, name: String, salary: Int, organizationId: Int) : this(id, name, salary) {
+        this.organizationId = organizationId
+    }
+}
